02cec8ddf91dd5ec23a15b037bc010022a2fef7d
Shairyar <sj82989p@pace.edu>
2024-03-02 15:18:53 -0500

    lab4

--- login.php
+++ login.php
@@ -11,3 +11,20 @@
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
   ];
+
+
+
+
+
+  <?php
+  require_once 'login.php';
+
+  try
+  {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+  }
+  catch (PDOException $e)
+  {
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+  }
+?>
